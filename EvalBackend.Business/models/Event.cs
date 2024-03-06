99c9c991452f699f57f8e638e4cbb63f6cfbd7d0
Colin Alves <alves.colin@icloud.com>
2024-03-06 13:24:13 +0100

    Add event model - title, description, date, location

---
+++ EvalBackend.Business/models/Event.cs
@@ -0,0 +1,10 @@
+namespace EvalBackend.Domain.models;
+
+public class Event
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public DateTime Date { get; set; }
+    public string Location { get; set; } = default!;
+}
